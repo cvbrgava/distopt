fde918418aaa195f178dccdbbe16ad4956f6cf03
Bhargava Chintalapati <cvbrgava@gmail.com>
2013-06-22 22:18:00 +0530

    Reduction in memory usage for dp knapsack

--- assign1/knapsack/solver.py
+++ assign1/knapsack/solver.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-
 def dynamicprog( weights , values, volume ):
 	for index in range( len( weights ) ) :
 		if index == 0 :
-			old = [ (0,[0]) if weights[index] > vol else (values[index], [1]) for vol in range( volume + 1 )]
-                	new = old 
-		else :
-			old = new
-        		new = [ (old[vol][0], old[vol][1]+[0])  for vol in range( volume + 1 ) if vol < weights[ index ]]+[ (old[vol][0], old[vol][1]+ [0]) if (old[vol][0]) > (values[ index ] + old[vol - weights[ index ] ][0])  else (values[ index ] + old[vol - weights[ index ] ][0], old[vol - weights[ index ] ][1]+[1])  for vol in range( volume + 1 ) if vol >= weights[ index ] ]
-        return new[ -1 ][ 0 ] , new[ -1 ] [ 1 ] 
+			old = [ (0,[0]) if weights[index] > vol else (values[index], [1]) for vol in range( volume + 1 )]                	
+		else :			
+        		old = [ (old[vol][0], old[vol][1]+[0])  for vol in range( volume + 1 ) if vol < weights[ index ]]+[ (old[vol][0], old[vol][1]+ [0]) if (old[vol][0]) > (values[ index ] + old[vol - weights[ index ] ][0])  else (values[ index ] + old[vol - weights[ index ] ][0], old[vol - weights[ index ] ][1]+[1])  for vol in range( volume + 1 ) if vol >= weights[ index ] ]
+        return old[ -1 ][ 0 ] , old[ -1 ] [ 1 ] 
 
 
 def solveIt(inputData):
